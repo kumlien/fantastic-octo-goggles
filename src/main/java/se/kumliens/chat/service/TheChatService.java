54ba1de21239db2e7fedb9e0b2284eadccddfc61
Svante Kumlien <svante.kumlien@gmail.com>
2023-11-06 21:57:08 +0100

    add langchain4j basics

--- src/main/java/se/kumliens/chat/service/TheChatService.java
+++ src/main/java/se/kumliens/chat/service/TheChatService.java
@@ -2,21 +2,70 @@ package se.kumliens.chat.service;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.BrowserCallable;
-import org.tinylog.Logger;
-import reactor.core.publisher.Flux;
+import dev.langchain4j.memory.chat.TokenWindowChatMemory;
 
-@AnonymousAllowed
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.model.openai.OpenAiTokenizer;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.TokenStream;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
 @BrowserCallable
-public class TheChatService implements ChatService{
-    @Override
-    public String chat(String chatId, String message) {
-        Logger.info("Dummy chat service, I'm a dummy!!");
-        return "I'm a dummy chat service and you said '" + message + "'";
+@AnonymousAllowed
+@Service
+public class TheChatService implements ChatService {
+
+    @Value("${openai.api.key}")
+    private String OPENAI_API_KEY;
+    private Assistant assistant;
+    private StreamingAssistant streamingAssistant;
+
+    interface Assistant {
+        String chat(String message);
     }
 
-    @Override
-    public Flux<String> chatStream(String chatId, String message) {
-        Logger.info("Dummy Streaming chat service with chatId {}", chatId);
-        return Flux.just("I'm a\n", "dummy\n", "streaming\n", "chat service\n", "and you said\n", message);
+    interface StreamingAssistant {
+        TokenStream chat(String message);
+    }
+
+    @PostConstruct
+    public void init() {
+
+        var memory = TokenWindowChatMemory.withMaxTokens(2000, new OpenAiTokenizer("gpt-3.5-turbo"));
+
+        assistant = AiServices.builder(Assistant.class)
+                .chatLanguageModel(OpenAiChatModel.withApiKey(OPENAI_API_KEY))
+                .chatMemory(memory)
+                .build();
+
+        streamingAssistant = AiServices.builder(StreamingAssistant.class)
+                .streamingChatLanguageModel(OpenAiStreamingChatModel.withApiKey(OPENAI_API_KEY))
+                .chatMemory(memory)
+                .build();
+    }
+
+    public String chat(String message) {
+        return assistant.chat(message);
+    }
+
+    public Flux<String> chatStream(String message) {
+        Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer();
+
+        streamingAssistant.chat(message)
+                .onNext(sink::tryEmitNext)
+                .onComplete(response -> {
+                    System.out.println("On complete: " + response.content().text());
+                    sink.tryEmitComplete();
+                })
+                .onError(sink::tryEmitError)
+                .start();
+
+        return sink.asFlux();
     }
 }
