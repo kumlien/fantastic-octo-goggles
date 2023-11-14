8024c1d3c4c98f8ab4dce85cb3ccc2a6f6b56c47
Svante Kumlien <svante.kumlien@gmail.com>
2023-11-14 22:10:55 +0100

    Switched to azure-openai

--- src/main/java/se/kumliens/chat/service/TheChatService.java
+++ src/main/java/se/kumliens/chat/service/TheChatService.java
@@ -3,28 +3,39 @@ package se.kumliens.chat.service;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.BrowserCallable;
 import dev.langchain4j.memory.chat.TokenWindowChatMemory;
-
-
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.model.azure.AzureOpenAiChatModel;
+import dev.langchain4j.model.azure.AzureOpenAiStreamingChatModel;
 import dev.langchain4j.model.openai.OpenAiTokenizer;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.TokenStream;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
+import se.kumliens.chat.tools.ExampleTool;
 
 @BrowserCallable
 @AnonymousAllowed
 @Service
 public class TheChatService implements ChatService {
 
-    @Value("${openai.api.key}")
-    private String OPENAI_API_KEY;
+    @Autowired
+    private ExampleTool exampleTool;
+
+    @Value("${azure.openai.api.key}")
+    private String AZURE_OPENAI_API_KEY;
+
+    @Value("${azure.openai.api.base_url}")
+    private String AZURE_OPENAI_API_BASE_URL;
+
+    @Value("${azure.openai.api.version}")
+    private String AZURE_OPENAI_API_VERSION;
+
     private Assistant assistant;
-    private StreamingAssistant streamingAssistant;
+
+    private StreamingAssistant azureStreamingAssistant;
 
     interface Assistant {
         String chat(String message);
@@ -40,13 +51,27 @@ public class TheChatService implements ChatService {
         var memory = TokenWindowChatMemory.withMaxTokens(2000, new OpenAiTokenizer("gpt-3.5-turbo"));
 
         assistant = AiServices.builder(Assistant.class)
-                .chatLanguageModel(OpenAiChatModel.withApiKey(OPENAI_API_KEY))
+                .chatLanguageModel(AzureOpenAiChatModel.builder()
+                        .apiKey(AZURE_OPENAI_API_KEY)
+                        .baseUrl(AZURE_OPENAI_API_BASE_URL)
+                        .apiVersion(AZURE_OPENAI_API_VERSION)
+                        .logRequests(true)
+                        .logResponses(true)
+                        .build())
                 .chatMemory(memory)
+                .tools(exampleTool)
                 .build();
 
-        streamingAssistant = AiServices.builder(StreamingAssistant.class)
-                .streamingChatLanguageModel(OpenAiStreamingChatModel.withApiKey(OPENAI_API_KEY))
+        azureStreamingAssistant = AiServices.builder(StreamingAssistant.class)
+                .streamingChatLanguageModel(AzureOpenAiStreamingChatModel.builder()
+                        .apiKey(AZURE_OPENAI_API_KEY)
+                        .baseUrl(AZURE_OPENAI_API_BASE_URL)
+                        .apiVersion(AZURE_OPENAI_API_VERSION)
+                        .logRequests(true)
+                        .logResponses(true)
+                        .build())
                 .chatMemory(memory)
+                .tools(exampleTool)
                 .build();
     }
 
@@ -57,7 +82,7 @@ public class TheChatService implements ChatService {
     public Flux<String> chatStream(String message) {
         Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer();
 
-        streamingAssistant.chat(message)
+        azureStreamingAssistant.chat(message)
                 .onNext(sink::tryEmitNext)
                 .onComplete(response -> {
                     System.out.println("On complete: " + response.content().text());
