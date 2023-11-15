8a40f69f98534770f34e52ced826edc76860241e
Svante Kumlien <svante.kumlien@gmail.com>
2023-11-15 16:32:51 +0100

    added tinylog dep and some minor stuff.

--- src/main/java/se/kumliens/chat/tools/ExampleTool.java
+++ src/main/java/se/kumliens/chat/tools/ExampleTool.java
@@ -3,6 +3,7 @@ package se.kumliens.chat.tools;
 import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
 import org.springframework.stereotype.Component;
+import org.tinylog.Logger;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,18 +13,19 @@ public class ExampleTool {
 
     @Tool(name = "AddingTool", value = "Used for arithmetic addition. The user provides two numbers which will be added together")
     public int add(int a, int b) {
-        System.out.println("Adding " + a + " and " + b + " together");
+        Logger.info("Adding '{}' and '{}' together", a, b);
         return a + b;
     }
 
     @Tool(name = "MultiplyTool", value = "Use for arithmetic multiplication of two numbers")
     public int multiply(int a, int b) {
+        Logger.info("Multiplying '{}' with '{}'", a, b);
         return a * b;
     }
 
     @Tool("This tool is used to get the current date and time in ISO-8601 format")
     public String getCurrentDateAndTime(@P("Dummy variable to avoid NP. Ignore this variable") String ignoreMe) {
-        System.out.println("Returning current local date with param " + ignoreMe);
+        Logger.info("Returning current local date with param '{}'", ignoreMe);
         return LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
     }
 
